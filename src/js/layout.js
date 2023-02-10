d0a10f0f559d609b3f2e3b048dfa037aaea22abc
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-02-10 00:56:07 +0000

    por revisar

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Characters } from "./views/characters";
+import { Charactersdetails } from "./views/charactersdetails";
+import { Planets } from "./views/planets";
+import { Planetsdetails} from "./views/planetsdetails";
 
-//create your first component
+import injectContext from "./store/appContext";
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div>
+		<>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
+				<Navbar />
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route exact path="/" component={Home} />
+						<Route exact path="/Characters" component={Characters} />
+						<Route exact path="/Charactersdetails/:id" component={Charactersdetails} />
+						<Route exact path="/Planets" component={Planets} />
+						<Route exact path="/Planetsdetails/:id" component={Planetsdetails} />
+						
 					</Routes>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
-		</div>
+		</>
 	);
 };
 
